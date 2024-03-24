ec82ea4c8a6fabff35434b23a0e57ea1abdd4679
Mafalda Vivas Matias <mafaldavivasmatias@tecnico.ulisboa.pt>
2024-03-24 19:32:12 +0000

    test: final verifications
    closes #106

--- frontend/tests/e2e/specs/enrollment/enrollment.js
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -13,9 +13,7 @@ describe('Enrollment', () => {
     const INITIAL_NUMBER = '0';
     const FINAL_NUMBER = '1';
 
-    //  As a member:  
-    //  - verify number of lines of activity table (3)         
-    //  - Verify number of enrollments in 1st activity (0)
+    //  As a member:           
     cy.demoMemberLogin();
 
     cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
@@ -23,19 +21,16 @@ describe('Enrollment', () => {
     cy.get('[data-cy="activities"]').click();
     cy.wait('@getInstitutions');
 
-    // check results
+    //  - verify number of lines of activity table (3)       
+    cy.get('[data-cy="memberActivitiesTable"] tbody tr').should('have.length', 3);
+    //  - Verify number of enrollments in 1st activity (0)
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-      .should('have.length', 3)
-      .eq(0)
-      .children()
-      .should('have.length', 12);
-    cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-      .eq(0).children().eq(3).should('contain', INITIAL_NUMBER)
+      .eq(0).children().eq(3).should('contain', INITIAL_NUMBER);
     cy.logout();
 
-    //  As a volunteer  
-    //  - enroll in first activity
+    //  As a volunteer:
     cy.demoVolunteerLogin();
+
     cy.intercept('POST', '/activities/*/enrollments').as('register');
     cy.intercept('GET', '/activities').as('getActivities');
     //  - enroll in first activity
@@ -45,13 +40,31 @@ describe('Enrollment', () => {
     cy.get('[data-cy="motivationInput"]').type(MOTIVATION);
     cy.get('[data-cy="saveEnrollment"]').click();
     cy.wait('@register');
+
     cy.logout();
 
     //  As a member:  
-    //  - verify number of enrollments in 1st activity (1)  
-    //  - select show enrollments in 1st activity
-    //  - verify number of lines of enrollments in 1st activity table (1) with correct motivation      
     cy.demoMemberLogin();
+
+    cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+    cy.intercept('GET', '/activities/*/enrollments').as('getEnrollments');
+    cy.get('[data-cy="institution"]').click();
+    cy.get('[data-cy="activities"]').click();
+    cy.wait('@getInstitutions');
+    //  - verify number of enrollments in 1st activity (1)  
+    cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+      .eq(0).children().eq(3).should('contain', FINAL_NUMBER);
+    //  - select show enrollments in 1st activity
+    cy.get('[data-cy="showEnrollments"]').first().click()
+    cy.wait('@getEnrollments');  
+    //  - verify number of lines of enrollments in 1st activity table (1) with correct motivation      
+    cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+      .should('have.length', 1)
+      .eq(0)
+      .children()
+      .eq(0)
+      .should('contain', MOTIVATION);
+
     cy.logout();
   });
 });
